ed9bc8ee18d9c393af4ca7acfc3929aeaff44f0c
lased <61lased61@gmail.com>
2022-07-24 22:02:26 +0300

    fix: fix foreign keys

--- src/migration/1658680906657-tracks.ts
+++ src/migration/1658680906657-tracks.ts
@@ -39,11 +39,17 @@ export class tracks1658680906657 implements MigrationInterface {
       true,
     );
     await queryRunner.query(`
-        INSERT INTO public.tracks(id, name, duration, "albumId", "artistId")
-            VALUES 
-                ('b6563c55-2ea3-4ce1-a8c1-6877e349bcf3', 'track 1', 120, 'b90bbee1-9617-48d9-b5ad-4dbe9c06e53e', '1f817a3d-24d1-4257-9b13-717495c0486d'),
-                ('b962dea7-031c-4bd2-9d76-68a1b4b0cee4', 'track 2', 135, '61918975-3c1b-4a9c-80f6-ff80c641a50e', '744024fb-9e45-4d04-818a-6ca1cf484602'),
-                ('5a9bb592-ba87-47bf-975e-4d49ecafed24', 'track 3', 234, '27b2e133-ec99-4194-9b24-b3b70d111506', 'f4dc23dd-a05b-4037-b321-3f876c3cca31')
+      ALTER TABLE ONLY public.tracks
+        ADD CONSTRAINT "FK_5c52e761792791f57de2fec342d" FOREIGN KEY ("albumId") REFERENCES public.albums(id) ON DELETE SET NULL;
+
+      ALTER TABLE ONLY public.tracks
+        ADD CONSTRAINT "FK_62f595181306916265849fced48" FOREIGN KEY ("artistId") REFERENCES public.artists(id) ON DELETE SET NULL;
+
+      INSERT INTO public.tracks(id, name, duration, "albumId", "artistId")
+        VALUES 
+          ('b6563c55-2ea3-4ce1-a8c1-6877e349bcf3', 'track 1', 120, 'b90bbee1-9617-48d9-b5ad-4dbe9c06e53e', '1f817a3d-24d1-4257-9b13-717495c0486d'),
+          ('b962dea7-031c-4bd2-9d76-68a1b4b0cee4', 'track 2', 135, '61918975-3c1b-4a9c-80f6-ff80c641a50e', '744024fb-9e45-4d04-818a-6ca1cf484602'),
+          ('5a9bb592-ba87-47bf-975e-4d49ecafed24', 'track 3', 234, '27b2e133-ec99-4194-9b24-b3b70d111506', 'f4dc23dd-a05b-4037-b321-3f876c3cca31')
     `);
   }
 
